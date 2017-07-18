22b7b51e01456e3ba65c1c25257dbae878d4ebb9
wanderleirx <wanderleirx@gmail.com>
2017-07-18 20:55:12 -0300

    first commit

---
+++ front-end/src/components/products/Products.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <div class="col l12">
+            <h3>Produtos</h3>
+            <div class="divider"></div>
+             <formProducts></formProducts> 
+             <tableProducts></tableProducts> 
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import tableProducts from './tableProducts.vue'
+    import formProducts from './formProducts.vue'
+
+    export default{
+        components:{
+            tableProducts, formProducts
+        }
+        
+    }
+</script>
